2766a2c02a544cbf7d05b1de2285291925df3440
Maggie Tieu <maggietieu@ufl.edu>
2021-04-05 00:11:21 -0400

    Edited general layout margins

--- Auth0 App/client/src/views/CreateEvent.js
+++ Auth0 App/client/src/views/CreateEvent.js
@@ -13,55 +13,66 @@ const CreateEvent = () => {
 
       <Form>
         <Form.Group as={Row} controlId="EventName">
+          <Col sm={2}></Col>
           <Form.Label column sm={2}>
             Event Name
           </Form.Label>
-          <Col sm={10}>
+          <Col sm={6}>
             <Form.Control type="event-name" placeholder="Event Name" />
           </Col>
+          <Col sm={2}></Col>
         </Form.Group>
 
         <Form.Group as={Row} controlId="EventDate">
+        <Col sm={2}></Col>
           <Form.Label column sm={2}>
             Date
           </Form.Label>
-          <Col sm={10}>
+          <Col sm={6}>
             <Form.Control type="date" placeholder="Date" />
           </Col>
+          <Col sm={2}></Col>
         </Form.Group>
 
         <Form.Group as={Row} controlId="StartTime">
+          <Col sm={2}></Col>
           <Form.Label column sm={2}>
             Start Time
           </Form.Label>
-          <Col sm={10}>
+          <Col sm={6}>
             <Form.Control type="start-time" placeholder="Start Time" />
           </Col>
+          <Col sm={2}></Col>  
         </Form.Group>
 
         <Form.Group as={Row} controlId="EndTime">
+          <Col sm={2}></Col>
           <Form.Label column sm={2}>
             End Time
           </Form.Label>
-          <Col sm={10}>
+          <Col sm={6}>
             <Form.Control type="end-time" placeholder="End Time" />
           </Col>
+          <Col sm={2}></Col>
         </Form.Group>
 
         <Form.Group as={Row} controlId="Location">
+          <Col sm={2}></Col>
           <Form.Label column sm={2}>
             Location
           </Form.Label>
-          <Col sm={10}>
+          <Col sm={6}>
             <Form.Control type="location" placeholder="Location" />
           </Col>
+          <Col sm={2}></Col>
         </Form.Group>
 
         <Form.Group as={Row} controlId="Description">
+          <Col sm={2}></Col>
           <Form.Label column sm={2}>
             Description
           </Form.Label>
-          <Col sm={10}>
+          <Col sm={6}>
             <Form.Control
               as="textarea"
               type="description"
@@ -69,6 +80,7 @@ const CreateEvent = () => {
               rows={3}
             />
           </Col>
+          <Col sm={2}></Col>
         </Form.Group>
 
         {/* Button has no function */}
