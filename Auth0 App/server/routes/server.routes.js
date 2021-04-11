c755608a00a2b863e18807518b9af4c1f4a7ae4c
Cole McMullen <coledmcmullen@gmail.com>
2021-04-11 19:39:34 -0400

    Added Event functionality Also simplified update queries for User interfaces

--- Auth0 App/server/routes/server.routes.js
+++ Auth0 App/server/routes/server.routes.js
@@ -1,10 +1,11 @@
 const controllers = require('../controllers/server.controllers');
 const users = require('../models/server.users');
+const events = require('../models/server.events');
 const express = require('express');
 const router = express.Router();
 
 router.get('/users', (req, res, next) => {
-    users.find({}, 'email')
+    users.find({}, 'email given_name family_name points')
         .then(data => res.json(data))
         .catch(next)
 });
@@ -26,17 +27,9 @@ router.get('/users/name/:name', (req, res, next) => {
 
 router.post('/users/:id/inc', (req, res, next) => {
     var pnt;
-    users.findById(req.params.id, 'points', (err, doc) => {
-        if(err){
-            console.log(err);
-        } else {
-            pnt = doc.points;
-            pnt++;
-            users.findByIdAndUpdate(req.params.id, { points: pnt }, { new: true, select: 'points' })
-                .then(data => res.json(data))
-                .catch(next)
-        }
-    })
+    users.findByIdAndUpdate(req.params.id, { $inc: { points: 1 } })
+        .then(data => res.json(data))
+        .catch(next)
 });
 
 router.post('/users/:id/reset', (req, res, next) => {
@@ -45,5 +38,19 @@ router.post('/users/:id/reset', (req, res, next) => {
         .then(data => res.json(data))
         .catch(next)
 });
+
+router.post('/events/new', (req, res, next) => {
+    console.log(req.body.googleId);
+    var newEvent = new events({ googleId: req.body.googleId, attending: [] });
+    newEvent.save()
+        .then(data => res.json(data))
+        .catch(next)
+});
+
+router.post('/events/attend', (req, res, next) => {
+    events.findOneAndUpdate({ googleId: req.body.googleId }, { $addToSet: { attending: req.body.userId } })
+        .then(data => res.json(data))
+        .catch(next)
+})
 
 module.exports = router;
