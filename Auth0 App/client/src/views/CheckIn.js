5e27943957ec23441d0b7854584f66cd4d45951b
Maggie Tieu <maggietieu@ufl.edu>
2021-04-14 02:24:34 -0400

    Added CheckIn file & page route (Functionality not implemented yet))

---
+++ Auth0 App/client/src/views/CheckIn.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useHistory } from "react-router";
+import AppBar from "../components/AppBar.js";
+
+const CheckIn = () => {
+  return (
+    <div>
+      <AppBar />
+      <h1 style={{ textAlign: "center" }}>Current Events</h1>
+    </div>
+  );
+};
+
+export default CheckIn;
