40db3ba7762dc3c1aa724f1dbc52ebd52caaae45
jenny <jenny.xie@ufl.edu>
2021-04-14 10:19:38 -0400

    fixed things

--- Auth0 App/client/src/components/LogoutButton.js
+++ Auth0 App/client/src/components/LogoutButton.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
+import {
+  useAuth0
+} from "@auth0/auth0-react";
 
 const LogoutButton = () => {
-  const { logout, isAuthenticated } = useAuth0();
+  const {
+    logout,
+    isAuthenticated
+  } = useAuth0();
 
   return (
-    isAuthenticated && (
-      <button
-        style={{ color: "#151C48", background: "#f4ba00", fontWeight: "bold" }}
-        onClick={() => logout()}
-      >
-        Log Out
+    isAuthenticated && ( 
+      <button className = "btn-smaller" >
+      Log Out 
       </button>
     )
   );
