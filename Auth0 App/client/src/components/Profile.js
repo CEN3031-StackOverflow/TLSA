e533a6719733b0ca11d5bdd1867f43127ffe850c
Maggie Tieu <maggietieu@ufl.edu>
2021-04-15 15:11:45 -0400

    Minor front-end change on Profile.js

--- Auth0 App/client/src/components/Profile.js
+++ Auth0 App/client/src/components/Profile.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import JSONPretty from "react-json-pretty";
 
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
@@ -8,13 +9,13 @@ const Profile = () => {
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
-        {/* <h2>{user.name}</h2> */}
+        <h2>{user.name}</h2>
         <p>{user.email}</p>
-        {/* <JSONPretty data={user} /> */}
+        <JSONPretty data={user} />
         {/* {JSON.stringify(user, null, 2)} */}
       </div>
     )
-  )
-}
+  );
+};
 
 export default Profile;
