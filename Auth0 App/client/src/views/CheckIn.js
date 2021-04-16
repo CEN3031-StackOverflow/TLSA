abc2bf341ff637743dd58fc62fa9fcbcf7d8c9ef
Maggie Tieu <maggietieu@ufl.edu>
2021-04-16 03:52:56 -0400

    Displays GET list of events correctly!

--- Auth0 App/client/src/views/CheckIn.js
+++ Auth0 App/client/src/views/CheckIn.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import { useHistory } from "react-router";
 import AppBar from "../components/AppBar.js";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const CheckIn = () => {
   const { user, isAuthenticated } = useAuth0();
+  const [events, setEvents] = useState();
 
   var gapi = window.gapi;
   // If need be, when you create a new OAuth 2.0 Client ID, make sure to name it "auth2".
@@ -14,9 +15,8 @@ const CheckIn = () => {
     "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest",
   ];
   var SCOPES = "https://www.googleapis.com/auth/calendar.events";
-  var events = [{id: "not getting events list fast enough :("}];
 
-  function loadCurrentEvents(){
+  async function loadCurrentEvents(){
     gapi.load('client:auth2', () => {
       gapi.client.init({
         apiKey: API_KEY,
@@ -34,36 +34,34 @@ const CheckIn = () => {
           'singleEvents': true,
           'orderBy': 'startTime'
         }).then(response => {
-          events = response.result.items
+          setEvents(response.result.items)
           console.log('EVENTS: ', events)
         })
       })
     })
   }
   
-  (async () => {
-    await loadCurrentEvents();
-
-  })();
+  loadCurrentEvents();
 
   return (
     isAuthenticated && (
-      <div>
+    <div>
         <h1 style={{ textAlign: "center" }}>Current Events</h1>
         <b/>
         <h1></h1>
-        <ul>
-          {/* loadCurrentEvents() is completing too late... */}
-          {
-            events.map(function(event){
-              console.log(event.id);
-              return  <div>
-                        <h6>{event.id}</h6>
-                        <button>check in</button>
-                      </div>
-            })
-          }
-        </ul>
+        { events &&
+          <ul>
+            {
+              events.map(function(event){
+                console.log(event.id);
+                return  <div>
+                          <h6>{event.id}</h6>
+                          <button>check in</button>
+                        </div>
+              })
+            }
+          </ul>
+        }
       </div>
     )
   );
